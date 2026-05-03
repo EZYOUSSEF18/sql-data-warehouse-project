ffd24757983d07c9f063a1e12b575f5d321f32e3
EZYOUSSEF18 <136393819+EZYOUSSEF18@users.noreply.github.com>
2026-05-03 19:35:51 +0100

    Create ddl_bronze.sql in scripts directory

---
+++ scripts/ddl_bronze.sql
@@ -0,0 +1,4 @@
+-- Bronze Layer DDL (Data Definition Language)
+-- This file contains the SQL statements to create tables in the Bronze layer
+-- Bronze layer tables store raw, unmodified data extracted from source systems
+
